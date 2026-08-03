5abfeef56f133990c445324876ffc4e7704b30af
mukeshdani <mukeshdani00@gmail.com>
2026-08-03 19:37:08 +0530

    Remove OIDC authentication configuration from deploy pipeline and provider setup

--- parent/provider.tf
+++ parent/provider.tf
@@ -11,8 +11,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  # Enables OpenID Connect (OIDC) authentication mechanism for secure, 
-  # passwordless identity federation with the GitHub Actions runner.
-  use_oidc = true
 }
